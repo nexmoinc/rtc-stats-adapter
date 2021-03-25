53a442a4384909ab8a75d25e158936c6d064d014
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2021-03-25 11:59:19 +0300

    added jitter

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+## 1.1.0
+
+### Changes
+
+- Added additional fields to a schema:
+    - audioSentJitter
+    - audioRecvJitter
+    - networkMos
+- Adjusted validation rules for the output schema.
--- schema/output.js
+++ schema/output.js
@@ -1,7 +1,8 @@
 const Joi = require('joi');
 
 const metadataSchema = Joi.object({
-    legId: Joi.string().required()
+    legId: Joi.string().required(),
+    networkMos: Joi.number().positive().allow(0).required()
 })
 
 // Core set of RTC Stats that is implemented well X-Browser
@@ -9,35 +10,53 @@ const statsSchema = Joi.object({
     audioRecvBytes: Joi.number()
         .integer()
         .positive()
+        .allow(0)
         .required(),
 
     audioRecvPackets: Joi.number()
         .integer()
         .positive()
+        .allow(0)
         .required(),
 
     audioRecvPacketsLost: Joi.number()
         .integer()
         .positive()
+        .allow(0)
         .required(),
 
     audioRtt: Joi.number()
         .positive()
+        .allow(0)
         .required(),
 
     audioSentBytes: Joi.number()
         .integer()
         .positive()
+        .allow(0)
         .required(),
 
     audioSentPackets: Joi.number()
         .integer()
         .positive()
+        .allow(0)
         .required(),
 
     audioSentPacketsLost: Joi.number()
         .integer()
         .positive()
+        .allow(0)
+        .allow(null)
+        .required(),
+
+    audioSentJitter: Joi.number()
+        .positive()
+        .allow(0, null)
+        .required(),
+
+    audioRecvJitter: Joi.number()
+        .positive()
+        .allow(0, null)
         .required(),
 })
 
